3b4bb7904ba0c2d4562ebb2b08e6ba001318a405
Olivier Meunier <oli.meunier@gmail.com>
2022-05-11 11:04:15 +0200

    Comments on lorem ipsum

--- JavaScript/Asynchronous/fetch/lorem-ipsum/js/index.js
+++ JavaScript/Asynchronous/fetch/lorem-ipsum/js/index.js
@@ -14,10 +14,20 @@ function onSubmitForm(event)
 
     // On lance la requête vers l'API Bacon Ipsum
     fetch(`https://baconipsum.com/api/?type=all-meat&paras=${paras}&start-with-lorem=1&format=json`)
+
+        // Une fois qu'on reçoit la réponse, on parse le contenu du body en précisant qu'il s'agit de données au format JSON
         .then(response => response.json())
+
+        // Une fois les données parsées, on récupère un tableau de paragraphes de lorem ipsum
         .then(paragraphs => {
+
+            // On efface la zone sur la page
             container.innerHTML = '';
+
+            // On parcours les paragraphes et pour chaque paragraphe...
             for (const paragraph of paragraphs) {
+
+                // ... on concatène à ce qu'il y a déjà un nouveau paragraphe de texte !
                 container.innerHTML += `<p>${paragraph}</p>`; 
             }
         });
